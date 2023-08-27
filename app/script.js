c45394126130a901a4954f9feb5c54a5bcace538
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2023-08-27 12:42:42 -0300

    Create script.js

---
+++ app/script.js
@@ -0,0 +1,23 @@
+let initialAmount = 23000
+let monthlyAmount = 0
+let period = 2
+let total = 0
+
+async function fetchSelicRate(){
+
+    const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos?formato=json'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    calcInvestiment(data)
+}
+
+async function calcInvestiment(data){
+    const currentRate = Number(data[0].valor)
+    const monthlyRate = (1 + currentRate / 100)**(1/12)-1
+
+    for(let i=1; i <= period; i++){
+        let selicYield = (initialAmount * monthlyRate) 
+        initialAmount += selicYield
+    }
+}
