20e80250be950388918a1949288c18a7e8a8c10f
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2023-09-05 18:21:56 -0300

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,7 +1,12 @@
-let initialAmount = 23000
-let monthlyAmount = 0
-let period = 6
-let total = 0
+let initial_amount = document.querySelector('#initial_amount').value
+let monthly_amount = document.querySelector('#monthly_amount').value
+let period = document.querySelector('#period').value
+let amount_invested = document.querySelector('#amount-invested')
+let gross_total_amount = initial_amount
+let income_tax = 0
+let invest_interest_gain = 0
+let invest_value = 0
+let invest_net_total = 0
 
 async function fetchSelicRate(){
 
@@ -13,13 +18,21 @@ async function fetchSelicRate(){
 }
 
 async function calcInvestiment(data){
-    fetchSelicRate()
     
     const currentRate = Number(data[0].valor)
-    const monthlyRate = (1 + currentRate / 100)**(1/12)-1
+    const monthly_rate = (1 + currentRate / 100)**(1/12)-1
 
     for(let i=1; i <= period; i++){
-        let selicYield = initialAmount * monthlyRate
-        total = initialAmount += selicYield
+      if(!monthly_amount){
+        
+        let selic_yield = gross_total_amount  * monthly_rate
+        gross_total_amount = gross_total_amount += selic_yield
+      } 
+      else if(monthly_amount){
+        
+        let selic_yield = gross_total_amount  * monthly_rate
+        let yield_plus_montly_amount = selic_yield += monthly_amount
+        gross_total_amount = gross_total_amount += yield_plus_montly_amount
+      }
     }
 }
